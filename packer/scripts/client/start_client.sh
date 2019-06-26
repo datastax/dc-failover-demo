542b323fbcbfc2e4e0de2732d51f73dd9bb6084e
Michael Penick <michael.penick@datastax.com>
2019-06-26 16:04:36 -0400

    Load client fixes

--- packer/scripts/client/start_client.sh
+++ packer/scripts/client/start_client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-locust -f locustfile.py --host=http://$1:8080
+locust -f locustfile.py --host=http://$1
--- terraform/instances_client.tf
+++ terraform/instances_client.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "client_r1" {
   subnet_id = "${aws_subnet.r1_az1.id}"
   associate_public_ip_address = true
   key_name = "${aws_key_pair.key_r1.key_name}"
-  vpc_security_group_ids = ["${aws_security_group.sg_client_r1.id}"]
+  vpc_security_group_ids = ["${aws_security_group.sg_default_r1.id}", "${aws_security_group.sg_client_r1.id}"]
   depends_on = [aws_globalaccelerator_accelerator.demo_acc]
 
   provisioner "remote-exec" {
