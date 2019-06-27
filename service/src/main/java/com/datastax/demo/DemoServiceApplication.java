ff2ec7e105468ecee418596b8929f14526be04ed
Michael Penick <michael.penick@datastax.com>
2019-06-27 13:11:39 -0400

    Return useful errors from web services

--- service/src/main/java/com/datastax/demo/DemoServiceApplication.java
+++ service/src/main/java/com/datastax/demo/DemoServiceApplication.java
@@ -11,13 +11,32 @@ import com.datastax.oss.driver.api.core.CqlSession;
 import io.dropwizard.Application;
 import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
 import io.dropwizard.configuration.SubstitutingSourceProvider;
+import io.dropwizard.jersey.errors.ErrorMessage;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 public class DemoServiceApplication extends Application<DemoServiceConfiguration> {
     private static final Logger logger = LoggerFactory.getLogger(DemoServiceApplication.class);
+
+    private static class DemoExceptionMapper implements ExceptionMapper<Exception> {
+        @Override
+        public Response toResponse(Exception e) {
+            StringWriter stackTraceWriter = new StringWriter();
+            e.printStackTrace(new PrintWriter(stackTraceWriter));
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(new ErrorMessage(e.getMessage()))
+                    .type(MediaType.APPLICATION_JSON)
+                    .build();
+        }
+    }
 
     public static void main(final String[] args) throws Exception {
         new DemoServiceApplication().run(args);
@@ -41,6 +60,7 @@ public class DemoServiceApplication extends Application<DemoServiceConfiguration
 
         SchemaManager.createSchema(session, config);
 
+        environment.jersey().register(new DemoExceptionMapper());
         environment.jersey().register(new DemoResource1(new DemoDAO(session)));
         environment.jersey().register(new ShoppingResource(new ShoppingDAO(session)));
         environment.jersey().register(new HealthCheckResource());
