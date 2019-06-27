c9ba3d6a7cfcd49b0cf8e0adb839813ad85c44be
Michael Penick <michael.penick@datastax.com>
2019-06-27 16:06:48 -0400

    Clean up `wait_for_cassandra.sh` output

--- packer/scripts/cassandra/wait_for_cassandra.sh
+++ packer/scripts/cassandra/wait_for_cassandra.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-echo "Waiting for port 9042..."
-
 export PRIVATE_IP=$(ec2metadata --local-ipv4)
 
-if timeout 240 bash -c 'until echo > /dev/tcp/$PRIVATE_IP/9042; do sleep 2; done'; then
+if timeout 240 bash -c 'until (echo > /dev/tcp/$PRIVATE_IP/9042) 2> /dev/null; do echo "Waiting for port 9042..."; sleep 2; done'; then
     echo "Cassandra node bootstrapped at $PRIVATE_IP"
 else
     echo "Failed to start cassandra at $PRIVATE_IP"
